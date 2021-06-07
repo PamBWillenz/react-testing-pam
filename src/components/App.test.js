b8cd719519d5677c3e263fc021fd26a1dbcaf181
Pam Willenz <pam@positek.net>
2021-06-07 18:20:18 -0400

    define components, use flow

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,10 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./App";
 
+// eslint-disable-next-line no-undef
 test("App snapshot test", () => {
   const component = renderer.create(<App />);
   const tree = component.toJSON();
+  // eslint-disable-next-line no-undef
   expect(tree).toMatchSnapshot();
 });
--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import data from "../../data/courses.json";
 
 const numItems = data.length;
